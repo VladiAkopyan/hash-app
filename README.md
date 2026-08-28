65c151e67fb8081ccbdc354cdaa124d67bf9594f
Vladi Akopyan <vakopan686@gmail.com>
2026-08-28 17:26:20 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 🔐 Hash-App
 ### A program for creating password hashes
+This is a simple program that allows you to create password hashes from text. Different algorithm options are available to choose from.
+
+> **Note**<br>
+> This program performs only one task — hashing data. It does not provide any other security-related services.
+
+## Information
+**Developer** - Vladi Akopyan
--- scripts/UI/window.py
+++ scripts/UI/window.py
@@ -5,5 +5,11 @@ class Window(CTk):
         super().__init__()
 
         self.title("hash-app")
-        self.geometry('900x700')
+        self.geometry('1000x700')
         self.resizable(False, False)
+
+        self.sidebar = CTkFrame(self, width=400, height=700)
+        self.sidebar.pack(side='left')
+        self.sidebar.pack_propagate(False)
+
+        CTkLabel(self.sidebar, text='Hash-App', font=("San Francisco", 38, 'bold')).pack(side='top', pady=10) # Title
