374b657c666bc463cf76fa6332d5a0b029da68d8
Vladi Akopyan <vakopan686@gmail.com>
2026-08-28 18:34:32 +0300

    first commit

--- UI/window.py
+++ UI/window.py
@@ -1,13 +1,16 @@
 from customtkinter import *
 from datas.hash import *
 from scripts.hashing import *
+import pyperclip
 
 
-USER_TOTAL_HASH = "You haven't entered anything yet."
+
 
 class Window(CTk):
     def __init__(self):
         super().__init__()
+
+        self.USER_TOTAL_HASH = "You haven't entered anything yet."
 
         self.title("hash-app")
         self.geometry('1000x700')
@@ -34,37 +37,43 @@ class Window(CTk):
         self.input_salt_for_hash = CTkEntry(self, width=300, placeholder_text="Enter your salt for text (optional)", font=("San Francisco", 16, 'bold'))
         self.input_salt_for_hash.pack(side='top', pady=30)
 
-        self.total_hash = CTkLabel(self, text=f'Your hash:\n{USER_TOTAL_HASH}', font=("San Francisco", 12, 'bold'))
+        self.total_hash = CTkLabel(self, text=f'Your hash:\n{self.USER_TOTAL_HASH}', font=("San Francisco", 10, 'bold'))
         self.total_hash.pack(side='top', pady=30)
+
+        self.button_copy = CTkButton(self, text='Copy Hash', font=("San Francisco", 16, 'bold'), command=self.copy_hash)
+        self.button_copy.pack(side='top', pady=30)
 
 
         self.button_next = CTkButton(self, text='Start', font=("San Francisco", 16, 'bold'), command=self.click_start)
         self.button_next.pack(side='bottom', pady=30)
 
+    def copy_hash(self):
+        pyperclip.copy(self.USER_TOTAL_HASH)
+
     def click_start(self):
         if self.input_salt_for_hash.get() == "":
             if self.choice_type_hashing.get() == "SHA-224":
-                USER_TOTAL_HASH = hash224(self.input_text_for_hash.get())
-                self.total_hash.configure(text=f'Your hash:\n{USER_TOTAL_HASH}')
+                self.USER_TOTAL_HASH = hash224(self.input_text_for_hash.get())
+                self.total_hash.configure(text=f'Your hash:\n{self.USER_TOTAL_HASH}')
             if self.choice_type_hashing.get() == "SHA-256":
-                USER_TOTAL_HASH = hash256(self.input_text_for_hash.get())
-                self.total_hash.configure(text=f'Your hash:\n{USER_TOTAL_HASH}')
+                self.USER_TOTAL_HASH = hash256(self.input_text_for_hash.get())
+                self.total_hash.configure(text=f'Your hash:\n{self.USER_TOTAL_HASH}')
             if self.choice_type_hashing.get() == "SHA-384":
-                USER_TOTAL_HASH = hash384(self.input_text_for_hash.get())
-                self.total_hash.configure(text=f'Your hash:\n{USER_TOTAL_HASH}')
+                self.USER_TOTAL_HASH = hash384(self.input_text_for_hash.get())
+                self.total_hash.configure(text=f'Your hash:\n{self.USER_TOTAL_HASH}')
             if self.choice_type_hashing.get() == "SHA-512":
-                USER_TOTAL_HASH = hash512(self.input_text_for_hash.get())
-                self.total_hash.configure(text=f'Your hash:\n{USER_TOTAL_HASH}')
+                self.USER_TOTAL_HASH = hash512(self.input_text_for_hash.get())
+                self.total_hash.configure(text=f'Your hash:\n{self.USER_TOTAL_HASH}')
         else:
             if self.choice_type_hashing.get() == "SHA-224":
-                USER_TOTAL_HASH = hash224(self.input_text_for_hash.get(), self.input_salt_for_hash.get())
-                self.total_hash.configure(text=f'Your hash:\n{USER_TOTAL_HASH}')
+                self.USER_TOTAL_HASH = hash224(self.input_text_for_hash.get(), self.input_salt_for_hash.get())
+                self.total_hash.configure(text=f'Your hash:\n{self.USER_TOTAL_HASH}')
             if self.choice_type_hashing.get() == "SHA-256":
-                USER_TOTAL_HASH = hash256(self.input_text_for_hash.get(), self.input_salt_for_hash.get())
-                self.total_hash.configure(text=f'Your hash:\n{USER_TOTAL_HASH}')
+                self.USER_TOTAL_HASH = hash256(self.input_text_for_hash.get(), self.input_salt_for_hash.get())
+                self.total_hash.configure(text=f'Your hash:\n{self.USER_TOTAL_HASH}')
             if self.choice_type_hashing.get() == "SHA-384":
-                USER_TOTAL_HASH = hash384(self.input_text_for_hash.get(), self.input_salt_for_hash.get())
-                self.total_hash.configure(text=f'Your hash:\n{USER_TOTAL_HASH}')
+                self.USER_TOTAL_HASH = hash384(self.input_text_for_hash.get(), self.input_salt_for_hash.get())
+                self.total_hash.configure(text=f'Your hash:\n{self.USER_TOTAL_HASH}')
             if self.choice_type_hashing.get() == "SHA-512":
-                USER_TOTAL_HASH = hash512(self.input_text_for_hash.get(), self.input_salt_for_hash.get())
-                self.total_hash.configure(text=f'Your hash:\n{USER_TOTAL_HASH}')
+                self.USER_TOTAL_HASH = hash512(self.input_text_for_hash.get(), self.input_salt_for_hash.get())
+                self.total_hash.configure(text=f'Your hash:\n{self.USER_TOTAL_HASH}')
